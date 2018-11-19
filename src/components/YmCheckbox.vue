aa386c45ce45cc56d9ed7600abfa1c232c4138c4
翟盈盈 <zhaiyingying@qiyi.com>
2018-11-19 19:17:58 +0800

    update

---
+++ src/components/YmCheckbox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="layui-unselect layui-form-checkbox" :class="{'layui-form-checked':currentValue}" @click="changeCheckd">
+        <span>{{text}}</span>
+        <i class="layui-icon"></i>
+    </div>
+</template>
+<script>
+export default {
+  props: ['value', 'text'],
+  data() {
+    return {
+      selectShow: false,
+      currentValue: this.value
+    }
+  },
+  watch: {
+    value(val) {
+      this.currentValue = val
+    }
+  },
+  created() {},
+  methods: {
+    changeCheckd() {
+      this.currentValue = !this.currentValue
+      this.$emit('input', this.currentValue)
+    }
+  }
+}
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-block">
+    <blockquote v-for="(item,index) in menus" :key="index" class="layui-elem-quote layui-quote-nm left-block" @click="$options.filters.openRoute(item.menuUrl)">
+      <router-link :to="{path:item.menuUrl}" v-if="item.menuUrl && item.menuUrl.indexOf('/')==0 ">{{item.menuName}}</router-link>
+      <a :href="item.menuUrl" target="_blank" v-else>{{item.menuName}}</a>
+    </blockquote>
+  </div>
+</template>
+<script>
+import cacheHelper from '../utils/cacheHelper'
+export default {
+  meta: {
+    indexHide: true,
+    menuName: '快速导航',
+    sort: -1
+  },
+  data() {
+    return {
+      menus: []
+    }
+  },
+  created() {
+    this.menus = window.siteMenuItems.filter(e => e.indexHide !== true)
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+.home-block {
+  margin-top: 5px;
+}
+.left-block {
+  width: 150px;
+  float: left;
+  margin-left: 10px;
+  cursor: pointer;
+}
+</style>
